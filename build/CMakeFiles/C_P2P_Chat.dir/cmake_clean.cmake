110375a11fb05c8c567c414842e3d168d3dc228b
seeembad <moseyarthas@gmail.com>
2023-04-04 12:50:48 +0300

    reset

---
+++ build/CMakeFiles/C_P2P_Chat.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_P2P_Chat.dir/src/Chat.c.o"
+  "CMakeFiles/C_P2P_Chat.dir/src/Chat.c.o.d"
+  "CMakeFiles/C_P2P_Chat.dir/src/Clients.c.o"
+  "CMakeFiles/C_P2P_Chat.dir/src/Clients.c.o.d"
+  "CMakeFiles/C_P2P_Chat.dir/src/Network/Packet.c.o"
+  "CMakeFiles/C_P2P_Chat.dir/src/Network/Packet.c.o.d"
+  "CMakeFiles/C_P2P_Chat.dir/src/Network/Socket.c.o"
+  "CMakeFiles/C_P2P_Chat.dir/src/Network/Socket.c.o.d"
+  "CMakeFiles/C_P2P_Chat.dir/src/Utils/Interface.c.o"
+  "CMakeFiles/C_P2P_Chat.dir/src/Utils/Interface.c.o.d"
+  "CMakeFiles/C_P2P_Chat.dir/src/Utils/Utils.c.o"
+  "CMakeFiles/C_P2P_Chat.dir/src/Utils/Utils.c.o.d"
+  "CMakeFiles/C_P2P_Chat.dir/src/main.c.o"
+  "CMakeFiles/C_P2P_Chat.dir/src/main.c.o.d"
+  "C_P2P_Chat"
+  "C_P2P_Chat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_P2P_Chat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
